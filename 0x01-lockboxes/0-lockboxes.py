9cb00f3a5e2f012b2bd50af3155ba98cc5ec4ced
Victor Langat <provicml@gmail.com>
2024-01-11 00:23:05 +0300

    test brutus force

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -25,54 +25,22 @@ can be opened.
 #         return all(visited)
 #     return False
 
-# def canUnlockAll(boxes):
-#     """brute force through each box"""
-#     if not boxes:
-#         return False
-#     size, visited, idx = len(boxes), {}, 0
-
-#     for box in boxes:
-#         if len(box) == 0 or idx == 0:
-#             visited[idx] = -1
-#         for key in box:
-#             if key < size and key != idx:
-#                 visited[key] = key
-#         if len(visited) == size:
-#             return True
-#         idx += 1
-#     return False
-def append_to_array(src, boxes_len, boxes, arr_to_append):
-    """ Append keys to array """
-    for key in src:
-        if key < boxes_len and key > 0:
-            if key not in arr_to_append:
-                arr_to_append.append(key)
-            for k in boxes[key]:
-                if k not in arr_to_append:
-                    arr_to_append.append(k)
-
-
 def canUnlockAll(boxes):
-    """ Can Unlock All
+    """brute force through each box"""
+    if not boxes:
+        return False
+    size, visited, idx = len(boxes), {}, 0
 
-        Args:
-            boxes: a list of lists
-    """
-    boxes_len = len(boxes)
-
-
-
-    unloced_keys = [0]
-
-    for i in range(boxes_len):
-        if i in unloced_keys:
-            append_to_array(boxes[i], boxes_len, boxes, unloced_keys)
-
-    if not all(i in unloced_keys for i in range(boxes_len)):
-        return (False)
-
-    return (True)
-
+    for box in boxes:
+        if len(box) == 0 or idx == 0:
+            visited[idx] = -1
+        for key in box:
+            if key < size and key != idx:
+                visited[key] = key
+        if len(visited) == size:
+            return True
+        idx += 1
+    return False
 
 if __name__ == "__main__":
     boxes = [[], [2], [3], [4], []]
