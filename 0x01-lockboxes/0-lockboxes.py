94d16b706bd93586ca0a12b5185fc165be7eabf1
Victor Langat <provicml@gmail.com>
2024-01-10 23:50:30 +0300

    test not bxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -7,28 +7,86 @@ can be opened.
 """
 
 
+# def canUnlockAll(boxes):
+#     """depth first search"""
+#     if ((isinstance(boxes, list)) and len(boxes) > 0):
+#         visited = [False]*len(boxes)
+
+#         def dfs(box):
+#             visited[box] = True
+#             for key in boxes[box]:
+#                 if key == 0:
+#                     visited[box] = True
+#                 if not visited[key]:
+#                     dfs(key)
+
+#         dfs(0)
+
+#         return all(visited)
+#     return False
+
+# def canUnlockAll(boxes):
+#     """brute force through each box"""
+#     if not boxes:
+#         return False
+#     size, visited, idx = len(boxes), {}, 0
+
+#     for box in boxes:
+#         if len(box) == 0 or idx == 0:
+#             visited[idx] = -1
+#         for key in box:
+#             if key < size and key != idx:
+#                 visited[key] = key
+#         if len(visited) == size:
+#             return True
+#         idx += 1
+#     return False
+def append_to_array(src, boxes_len, boxes, arr_to_append):
+    """ Append keys to array """
+    for key in src:
+        if key < boxes_len and key > 0:
+            if key not in arr_to_append:
+                arr_to_append.append(key)
+            for k in boxes[key]:
+                if k not in arr_to_append:
+                    arr_to_append.append(k)
+
+
 def canUnlockAll(boxes):
-    """depth first search"""
-    # if ((isinstance(boxes, list)) and len(boxes) > 0):
-    #     visited = [False]*len(boxes)
+    """ Can Unlock All
 
-    #     def dfs(box):
-    #         visited[box] = True
-    #         for key in boxes[box]:
-    #             if not visited[key]:
-    #                 dfs(key)
+        Args:
+            boxes: a list of lists
+    """
+    if not boxes:
+        return False
+    if not isinstance(boxes, list):
+        return (False)
 
-    #     dfs(0)
+    boxes_len = len(boxes)
 
-    #     return all(visited)
-    return False
+    if boxes_len <= 1:
+        return (True)
+
+    unloced_keys = [0]
+
+    for i in range(boxes_len):
+        if i in unloced_keys:
+            if not isinstance(boxes[i], list):
+                return (False)
+            append_to_array(boxes[i], boxes_len, boxes, unloced_keys)
+
+    if not all(i in unloced_keys for i in range(boxes_len)):
+        return (False)
+
+    return (True)
 
 
 if __name__ == "__main__":
-    boxes = [[1], [2], [3], [4], []]
+    boxes = [[], [2], [3], [4], []]
     print(canUnlockAll(boxes))
 
-    boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
+    boxes = [[0, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
     print(canUnlockAll(boxes))
 
     boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
