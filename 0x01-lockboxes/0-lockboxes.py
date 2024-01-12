447c20036b418072df0657e2a0e1361a224c34e2
Victor Langat <provicml@gmail.com>
2024-01-12 13:43:53 +0300

    test recursion soln

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -25,22 +25,54 @@ can be opened.
 #         return all(visited)
 #     return False
 
-def canUnlockAll(boxes):
-    """brute force through each box"""
-    if not boxes:
-        return False
-    size, visited, idx = len(boxes), {}, 0
+# def canUnlockAll(box, visited):
+#     visited = [False]*len(boxes)
+#     for key in boxes[box]:
+#         if not visited[key]:
+#             canUnlockAll(key, visited)
+#     return all(visited)
 
-    for box in boxes:
-        if len(box) == 0 or idx == 0:
-            visited[idx] = -1
-        for key in box:
-            if key < size and key != idx:
-                visited[key] = key
-        if len(visited) == size:
-            return True
-        idx += 1
-    return False
+# def canUnlockAll(boxes):
+#    """Depth-first search version of the function"""
+#    visited = {0: True}
+#    stack = [0]
+#    while stack:
+#        idx = stack.pop()
+#        for key in boxes[idx]:
+#            if key not in visited and key < len(boxes):
+#                visited[key] = True
+#                stack.append(key)
+#    return len(visited) == len(boxes)
+
+def canUnlockAll(boxes):
+    """Depth-first search version of the function"""
+    visited = {0: True}
+    stack = [0]
+    while stack:
+        idx = stack.pop()
+        for key in boxes[idx]:
+            if key not in visited and key < len(boxes):
+                visited[key] = True
+                stack.append(key)
+    return len(visited) == len(boxes)
+
+# def canUnlockAll(boxes):
+#     """brute force through each box"""
+#     if not boxes:
+#         return False
+#     size, visited, idx = len(boxes), {}, 0
+
+#     for box in boxes:
+#         if len(box) == 0 or idx == 0:
+#             visited[idx] = -1
+#         for key in box:
+#             if key < size and key != idx:
+#                 visited[key] = key
+#         if len(visited) == size:
+#             return True
+#         idx += 1
+#     return False
+
 
 if __name__ == "__main__":
     boxes = [[], [2], [3], [4], []]
@@ -50,4 +82,14 @@ if __name__ == "__main__":
     print(canUnlockAll(boxes))
 
     boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
+    print(canUnlockAll(boxes))
+    boxes = []
+
+    keys = []
+    for n in range(1, 1000):
+        keys = []
+        for m in range(1, 1000):
+            keys.append(m)
+        boxes.append(keys)
+
     print(canUnlockAll(boxes))
