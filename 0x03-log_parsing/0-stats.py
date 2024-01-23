3fe9639978f8ee50512c4054f769287f856784cf
Victor Langat <provicml@gmail.com>
2024-01-24 00:31:59 +0300

    test mens for pycodestyle fixed

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -17,8 +17,8 @@ def print_output(stats, size):
     print(f"File size: {size}")
 
     for i in sorted(stats):
-        if stats[i]:
-            print(f"{i}: {stats[i]}")
+        # if stats[i]:
+        print(f"{i}: {stats[i]}")
 
 
 if __name__ == "__main__":
