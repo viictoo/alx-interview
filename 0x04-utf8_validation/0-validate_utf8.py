3b41242cb9c25cf1762ecabfa9c94a4b57bc1f04
Victor Langat <provicml@gmail.com>
2024-01-31 00:26:30 +0300

    test solution: use mask and bit shift

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -9,6 +9,8 @@ def validUTF8(data: List) -> bool:
     Returns:
         boolean: True if valid utf8 otherwise false
     """
+    # fails 3 checks
+
     # bitSeq = 0
     # for b in data:
     #     b = bin(b).replace('0b', '').rjust(8, '0')
@@ -20,14 +22,37 @@ def validUTF8(data: List) -> bool:
     #         bitSeq = len(b.split('0')[0]) - 1
     # return True
 
-    try:
-        for b in data:
-            if type(b) is not int:
+    # fails 3 checks:
+    # try:
+    #     for b in data:
+    #         if type(b) is not int:
+    #             return False
+    #         b.to_bytes(1, 'big').decode('utf-8')
+    #     return True
+    # except Exception:
+    #     return False
+
+    def check(num):
+        mask = 1 << (8 - 1)  # 10000000
+        i = 0
+        while num & mask:  # 11000110 & 100000
+            mask >>= 1
+            i += 1
+        return i
+
+    i = 0
+    while i < len(data):
+        j = check(data[i])
+        k = i + j - (j != 0)
+        i += 1
+        if j == 1 or j > 4 or k >= len(data):
+            return False
+        while i < len(data) and i <= k:
+            cur = check(data[i])
+            if cur != 1:
                 return False
-            b.to_bytes(1, 'big').decode('utf-8')
-        return True
-    except Exception:
-        return False
+            i += 1
+    return True
 
 
 if __name__ == "__main__":
@@ -47,6 +72,4 @@ if __name__ == "__main__":
     data = [229, 65, 127, 256]
     print(validUTF8(data))  # False
     data = []
-    print(validUTF8(data))  # False
-    data = ['a']
     print(validUTF8(data))  # False
